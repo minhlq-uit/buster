f593855b749d2a6f3b4c8eb7ee3da032c4572a9e
Nguyễn Ngọc Thái Nguyên <19521917@users.noreply.github.com>
2021-12-02 14:20:08 +0700

    test Mylist

--- src/pages/MyList/MyList.js
+++ src/pages/MyList/MyList.js
@@ -1,8 +1,11 @@
+import ListCard from "./ListCard/ListCard";
 import "./myList.scss";
 export default function News() {
   return (
     <div className="myList">
       <div className="main-title">My List</div>
+      <ListCard id="576845" category="movie" />
+      <ListCard id="576845" category="movie" />
     </div>
   );
 }
